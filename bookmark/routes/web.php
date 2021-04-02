b28aed359e27f98f57ab5e55a1a3516ff0521a01
Sam Janvey <sjanvey@gmail.com>
2021-04-01 21:42:47 -0400

    database lecture work

--- bookmark/routes/web.php
+++ bookmark/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/support', [PageController::class, 'support']);
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/search', [BookController::class, 'search']);
 
-// Make sure the create route comes before `/books/{slug?}` so it takes precedence
+// Make sure the create route comes before `/books/{slug}` so it takes precedence
 Route::get('/books/create', [BookController::class, 'create']);
 
 // Note the use of the post method in this route
@@ -19,3 +19,29 @@ Route::post('/books', [BookController::class, 'store']);
 Route::get('/books/{slug}', [BookController::class, 'show']);
 Route::get('/search/{category}/{subcategory}', [BookController::class, 'search']);
 Route::get('/list', [BookController::class, 'list']);
+
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
