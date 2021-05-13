338cf7eeea54489cbafd413d5920e6eed0d2c3d3
Sam Janvey <sjanvey@gmail.com>
2021-05-12 22:18:28 -0400

    added debug route

--- p3/routes/web.php
+++ p3/routes/web.php
@@ -5,29 +5,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 
-
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', [DashboardController::class, 'index']);
-
-    Route::get('/projects', [ProjectController::class, 'list']);
-
-    Route::get('/{slug}', [DashboardController::class, 'show']);
-
-    // CREATE
-    Route::post('/projects', [ProjectController::class, 'store']);
-
-    // READ
-    Route::get('/projects/{id}', [ProjectController::class, 'show']);
-
-    //UPDATE
-    Route::put('/projects/{id}', [ProjectController::class, 'update']);
-
-    // DELETE
-    Route::delete('projects/{id}', [ProjectController::class, 'destroy']);
-}); 
-
-
-
 Route::get('/debug', function () {
 
     $debug = [
@@ -53,3 +30,23 @@ Route::get('/debug', function () {
 
     dump($debug);
 });
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [DashboardController::class, 'index']);
+
+    Route::get('/projects', [ProjectController::class, 'list']);
+
+    Route::get('/{slug}', [DashboardController::class, 'show']);
+
+    // CREATE
+    Route::post('/projects', [ProjectController::class, 'store']);
+
+    // READ
+    Route::get('/projects/{id}', [ProjectController::class, 'show']);
+
+    //UPDATE
+    Route::put('/projects/{id}', [ProjectController::class, 'update']);
+
+    // DELETE
+    Route::delete('projects/{id}', [ProjectController::class, 'destroy']);
+}); 
